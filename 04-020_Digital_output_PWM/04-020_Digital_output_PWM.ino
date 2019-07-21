990bc568a5011790a26a6a8eadd1cdddbd0b3b04
Peter Dalmaris <peter@futureshock.com.au>
2019-07-22 08:36:19 +1000

    Added schematic for 4.30.

--- 04-020_Digital_output_PWM/04-020_Digital_output_PWM.ino
+++ 04-020_Digital_output_PWM/04-020_Digital_output_PWM.ino
@@ -47,7 +47,7 @@
  
 const byte led_gpio = 32; // the PWM pin the LED is attached to
 int brightness = 0;    // how bright the LED is
-int fadeAmount = 5;    // how many points to fade the LED by
+int fadeAmount = 1;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -56,7 +56,12 @@ void setup() {
   // Initialize channels
   // channels 0-15, resolution 1-16 bits, freq limits depend on resolution
   // ledcSetup(uint8_t channel, uint32_t freq, uint8_t resolution_bits);
-  ledcSetup(0, 4000, 8); // 12 kHz PWM, 8-bit resolution
+  ledcSetup(0, 4000, 2); // 4 kHz PWM, 8-bit resolution
+
+  //2^2 => 4
+  // 2^3 => 8
+  // 2^8 => 256
+  // 2^12 => 4,096
 }
 
 // the loop routine runs over and over again forever:
@@ -67,9 +72,9 @@ void loop() {
   brightness = brightness + fadeAmount;
 
   // reverse the direction of the fading at the ends of the fade:
-  if (brightness <= 0 || brightness >= 255) {
+  if (brightness <= 0 || brightness >= 3) {
     fadeAmount = -fadeAmount;
   }
   // wait for 30 milliseconds to see the dimming effect
-  delay(30);
+  delay(1000);
 }
